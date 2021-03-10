4420f7760c595c69bac73110f7b313d0fb60e740
謝榮睿 <louis61619@insynerger.com>
2021-03-10 16:34:01 +0800

    first commit

---
+++ precache-manifest.6f0ac71c6b7200cd6347a6ee1b34845b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f3b9c3be845b11521c",
+    "url": "css/app.529df4b1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "622e08b952fb75910b4eb2e17981e0a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "d8f3b9c3be845b11521c",
+    "url": "js/app.cf91dbe1.js"
+  },
+  {
+    "revision": "9442ce73c13e21e8e928",
+    "url": "js/chunk-vendors.a3fd750e.js"
+  },
+  {
+    "revision": "3868a87186592ee3caa21fc69400dc7f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
